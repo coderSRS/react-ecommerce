28f36ccf182eb3be22a696d0e343ff3c4501a5df
shihab-shuvo <coder.srs@gmail.com>
2020-08-28 01:29:41 +0600

    shopPage, collection-privew,collection-item components has been added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
+import shopPage from "./pages/shopPage/shopPage.component";
 
 import "./App.css";
 
@@ -17,7 +18,7 @@ class App extends React.Component {
       <div className='App'>
         <Switch>
           <Route exact path='/' component={HomePage} />
-          <Route path='/hats' component={HatsPage} />
+          <Route path='/shop' component={shopPage} />
         </Switch>
       </div>
     );
